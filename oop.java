6e6d6cc2f5742843b8c5c185d200b6ffa52cc79e
thinguyen135 <thipro135@gmail.com>
2025-11-01 20:39:43 +0700

    Update output to include additional text in print

--- oop.java
+++ oop.java
@@ -3,7 +3,7 @@ public class oop{
         User u= new User();
         u.set_name("Thi");;
         u.set_membership(User.Membership.Silver);
-        System.out.println("name:"+u.get_name());
+        System.out.println("name:"+u.get_name()+" nha");
         System.out.println(u.get_membership());
     }
 }
